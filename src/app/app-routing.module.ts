5189abe5a2e8ed346003c8e9ec73ef90a5d80fc6
Mint <gryvcio@gmail.com>
2019-05-31 14:18:05 +0200

    Update to angular 8

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './modules/home/home.module#HomeModule' },
-  { path: 'poradnia', loadChildren: './modules/poradnia/poradnia.module#PoradniaModule' },
-  { path: 'oferta', loadChildren: './modules/oferta/oferta.module#OfertaModule' },
-  { path: 'metamorfozy', loadChildren: './modules/metamorfozy/metamorfozy.module#MetamorfozyModule' },
-  { path: 'ciekawostki', loadChildren: './modules/ciekawostki/ciekawostki.module#CiekawostkiModule' },
-  { path: 'kontakt', loadChildren: './modules/kontakt/kontakt.module#KontaktModule' }
+  { path: '', loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule) },
+  { path: 'poradnia', loadChildren: () => import('./modules/poradnia/poradnia.module').then(m => m.PoradniaModule) },
+  { path: 'oferta', loadChildren: () => import('./modules/oferta/oferta.module').then(m => m.OfertaModule) },
+  { path: 'metamorfozy', loadChildren: () => import('./modules/metamorfozy/metamorfozy.module').then(m => m.MetamorfozyModule) },
+  { path: 'ciekawostki', loadChildren: () => import('./modules/ciekawostki/ciekawostki.module').then(m => m.CiekawostkiModule) },
+  { path: 'kontakt', loadChildren: () => import('./modules/kontakt/kontakt.module').then(m => m.KontaktModule) }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes, { useHash: true })],
